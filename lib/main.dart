0dc9011d63fe12b107f707aa29cc50150ff5daea
Moiz Zulfiqar <moiz.zulfiqar@icloud.com>
2025-05-03 20:49:52 +0500

    Authentication v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'dart:async';
 import './screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -12,9 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: LoginScreen());
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,29 +1,29 @@
 import 'dart:developer';
 import '../services/auth_service.dart';
-import 'signup_screen.dart';
+import 'login_screen.dart';
 import 'home_screen.dart';
 import '../widgets/button.dart';
 import '../widgets/textfield.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignupScreenState extends State<SignupScreen> {
   final _auth = AuthService();
 
-  bool _isLoading = false;
-
+  final _name = TextEditingController();
   final _email = TextEditingController();
   final _password = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
+    _name.dispose();
     _email.dispose();
     _password.dispose();
   }
@@ -36,9 +36,17 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           children: [
             const Spacer(),
-            const Text("Login",
+            const Text("Signup",
                 style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500)),
-            const SizedBox(height: 50),
+            const SizedBox(
+              height: 50,
+            ),
+            CustomTextField(
+              hint: "Enter Name",
+              label: "Name",
+              controller: _name,
+            ),
+            const SizedBox(height: 20),
             CustomTextField(
               hint: "Enter Email",
               label: "Email",
@@ -48,33 +56,20 @@ class _LoginScreenState extends State<LoginScreen> {
             CustomTextField(
               hint: "Enter Password",
               label: "Password",
+              isPassword: true,
               controller: _password,
             ),
             const SizedBox(height: 30),
             CustomButton(
-              label: "Login",
-              onPressed: _login,
-            ),
-            const SizedBox(height: 5),
-            _isLoading ? const CircularProgressIndicator() : CustomButton(
-              label: "Sign in with Google",
-              onPressed: () async {
-                setState(() {
-                  _isLoading = true;
-                }); 
-                await _auth.loginWithGoogle();
-                setState(() {
-                  _isLoading = false;
-                }); 
-              },
+              label: "Signup",
+              onPressed: _signup,
             ),
             const SizedBox(height: 5),
             Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               const Text("Already have an account? "),
               InkWell(
-                onTap: () => goToSignup(context),
-                child:
-                    const Text("Signup", style: TextStyle(color: Colors.red)),
+                onTap: () => goToLogin(context),
+                child: const Text("Login", style: TextStyle(color: Colors.red)),
               )
             ]),
             const Spacer()
@@ -84,9 +79,9 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  goToSignup(BuildContext context) => Navigator.push(
+  goToLogin(BuildContext context) => Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => const SignupScreen()),
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
       );
 
   goToHome(BuildContext context) => Navigator.push(
@@ -94,12 +89,11 @@ class _LoginScreenState extends State<LoginScreen> {
         MaterialPageRoute(builder: (context) => const HomeScreen()),
       );
 
-  _login() async {
+  _signup() async {
     final user =
-        await _auth.loginUserWithEmailAndPassword(_email.text, _password.text);
-
+        await _auth.createUserWithEmailAndPassword(_email.text, _password.text);
     if (user != null) {
-      log("User Logged In");
+      log("User Created Succesfully");
       goToHome(context);
     }
   }
